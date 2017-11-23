945e6e041ddfb5c5dc9b1ff7dd2c91e0f96fc42e
Jason Watmore <jason.watmore@tabcorp.com.au>
2017-11-23 17:20:53 +1100

    Updated to Angular 5.0.3

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -16,51 +16,51 @@ module.exports = {
 
     module: {
         rules: [
-          {
-              test: /\.ts$/,
-              loaders: [
-                {
-                    loader: 'awesome-typescript-loader',
-                    options: { configFileName: helpers.root('src', 'tsconfig.json') }
-                }, 'angular2-template-loader'
-              ]
-          },
-          {
-              test: /\.html$/,
-              loader: 'html-loader'
-          },
-          {
-              test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
-              loader: 'file-loader?name=assets/[name].[hash].[ext]'
-          },
-          {
-              test: /\.css$/,
-              exclude: helpers.root('src', 'app'),
-              loader: ExtractTextPlugin.extract({ fallbackLoader: 'style-loader', loader: 'css-loader?sourceMap' })
-          },
-          {
-              test: /\.css$/,
-              include: helpers.root('src', 'app'),
-              loader: 'raw-loader'
-          }
+            {
+                test: /\.ts$/,
+                loaders: [
+                    {
+                        loader: 'awesome-typescript-loader',
+                        options: { configFileName: helpers.root('src', 'tsconfig.json') }
+                    }, 'angular2-template-loader'
+                ]
+            },
+            {
+                test: /\.html$/,
+                loader: 'html-loader'
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
+                loader: 'file-loader?name=assets/[name].[hash].[ext]'
+            },
+            {
+                test: /\.css$/,
+                exclude: helpers.root('src', 'app'),
+                loader: ExtractTextPlugin.extract({ fallbackLoader: 'style-loader', loader: 'css-loader?sourceMap' })
+            },
+            {
+                test: /\.css$/,
+                include: helpers.root('src', 'app'),
+                loader: 'raw-loader'
+            }
         ]
     },
 
     plugins: [
-      // Workaround for angular/angular#11580
-      new webpack.ContextReplacementPlugin(
-        // The (\\|\/) piece accounts for path separators in *nix and Windows
-        /angular(\\|\/)core(\\|\/)(esm(\\|\/)src|src)(\\|\/)linker/,
-        helpers.root('./src'), // location of your src
-        {} // a map of your routes
-      ),
+        // Workaround for angular/angular#11580
+        new webpack.ContextReplacementPlugin(
+            // The (\\|\/) piece accounts for path separators in *nix and Windows
+            /angular(\\|\/)core(\\|\/)(@angular|esm5)/,
+            helpers.root('./src'), // location of your src
+            {} // a map of your routes
+        ),
 
-      new webpack.optimize.CommonsChunkPlugin({
-          name: ['app', 'vendor', 'polyfills']
-      }),
+        new webpack.optimize.CommonsChunkPlugin({
+            name: ['app', 'vendor', 'polyfills']
+        }),
 
-      new HtmlWebpackPlugin({
-          template: 'src/index.html'
-      })
+        new HtmlWebpackPlugin({
+            template: 'src/index.html'
+        })
     ]
 };
